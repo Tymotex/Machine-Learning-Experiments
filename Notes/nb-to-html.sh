557037737085e6850e05dc7080598c92fde21f4f
Tymotex <timzhang3@gmail.com>
2020-10-20 21:12:43 +1100

    Adding all HTML notes

---
+++ Notes/nb-to-html.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# For automated conversion of notebooks into html
+
+for eachNB in *.ipynb; do
+    jupyter nbconvert --to html $eachNB
+done
+
+for eachHTMLFile in *.html; do
+    if test $eachHTMLFile != index.html; then
+        echo "<a href=\"$eachHTMLFile\">$eachHTMLFile</a>" > index.html
+    fi
+done
