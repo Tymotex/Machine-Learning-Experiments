4b88451961e45c341e8d26d420640a49bc9d36cf
Tymotex <timzhang3@gmail.com>
2020-10-20 21:25:01 +1100

    Fixed notes links

--- Notes/nb-to-html.sh
+++ Notes/nb-to-html.sh
@@ -5,8 +5,10 @@ for eachNB in *.ipynb; do
     jupyter nbconvert --to html $eachNB
 done
 
+echo -n "" > index.html
 for eachHTMLFile in *.html; do
     if test $eachHTMLFile != index.html; then
-        echo "<a href=\"$eachHTMLFile\">$eachHTMLFile</a>" > index.html
+        echo "<h1>Quick Links</h1>" >> index.html
+        echo "<a href=\"$eachHTMLFile\">$eachHTMLFile</a>" >> index.html
     fi
 done
